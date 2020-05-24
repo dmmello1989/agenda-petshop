44624a1095f218a90b83e4cfe9990395d6578b15
Daniel Mello <dmmello1989@gmail.com>
2020-05-24 17:59:17 -0300

    transição do CRUD Clientes para GraphQL

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const customExpress = require('./config/custom-express')
+const { GraphQLServer } = require ('graphql-yoga')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/database/tabelas')
-
-const app = customExpress()
+const Operacoes = require('./infraestrutura/operations')
 
 conexao.connect(erro => {
   if (erro) {
@@ -14,6 +13,24 @@ conexao.connect(erro => {
   Tabelas.init(conexao)
 })
 
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000')
-})
+const Clientes = new Operacoes('cliente'); 
+
+const resolvers = {
+  Query: {
+    status: () => "Servidor rodando",
+    clientes: () => Clientes.lista()
+  },
+  Mutation: {
+    adicionarCliente: (root, params) => 
+      Clientes.adiciona(params)
+  }
+}
+
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: './schema.graphql'
+});
+
+servidor.start(() => {
+  console.log('Servidor ouvindo')
+});
